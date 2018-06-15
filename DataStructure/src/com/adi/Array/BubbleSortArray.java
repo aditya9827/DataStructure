4579e5892d08fa696bb736217d7f67c11e77fbb1
Aditya <adi@adi.com>
2018-06-15 12:39:41 +0530

    Bubble Sort

---
+++ DataStructure/src/com/adi/Array/BubbleSortArray.java
@@ -0,0 +1,44 @@
+package com.adi.Array;
+
+public class BubbleSortArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//int[] generatedArray = {100,90,10,2,4};
+		int[] generatedArray = new int[20];
+		// GenerateArray generate = new GenerateArray();
+		generatedArray = BasicArrayFunctionality.createArray(10);
+		
+		//generatedArray = {100,90};
+		BasicArrayFunctionality.printArray(generatedArray);
+		
+		// Time Complexity O(N^2)
+		for (int i = 0; i < generatedArray.length - 1; i++) {
+			for (int j = 0; j < generatedArray.length - i - 1; j++) {
+				if (generatedArray[j] > generatedArray[j + 1]) {
+					// swap(generatedArray[j], generatedArray[j+1]);
+					generatedArray[j + 1] = generatedArray[j] + generatedArray[j + 1];
+					generatedArray[j] = generatedArray[j + 1] - generatedArray[j];
+					generatedArray[j + 1] = generatedArray[j + 1] - generatedArray[j];
+				}
+			}
+			System.out.println("After " + (i+1) + "iteration");
+			BasicArrayFunctionality.printArray(generatedArray);
+		}
+		
+		System.out.println("After bubble sorting");
+		
+		BasicArrayFunctionality.printArray(generatedArray);
+
+	}
+
+	// private static void swap(int a, int b) {
+	// // TODO Auto-generated method stub
+	// b = a + b;
+	// a = b-a;
+	// b = b-a;
+	//
+	// }
+
+}
---
+++ DataStructure/src/com/adi/Array/OptimisedBubbleSort.java
@@ -0,0 +1,38 @@
+package com.adi.Array;
+
+public class OptimisedBubbleSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] generatedArray = new int[20];
+		
+		boolean swapped = false;
+		int arrayLength;
+		// GenerateArray generate = new GenerateArray();
+		generatedArray = BasicArrayFunctionality.createArray(10);
+		arrayLength = generatedArray.length;
+		
+		BasicArrayFunctionality.printArray(generatedArray);
+		
+		for(int i = 0; i < arrayLength - 1; i++) {
+			swapped = false;
+			for(int j = 0; j < arrayLength - i -1; j ++) {
+				if (generatedArray[j] > generatedArray[j + 1]) {
+					// swap(generatedArray[j], generatedArray[j+1]);
+					generatedArray[j + 1] = generatedArray[j] + generatedArray[j + 1];
+					generatedArray[j] = generatedArray[j + 1] - generatedArray[j];
+					generatedArray[j + 1] = generatedArray[j + 1] - generatedArray[j];
+					swapped = true;
+				}
+			}
+			if(!swapped) {
+				break;
+			}
+			System.out.println("After " + (i+1) + "iteration");
+			BasicArrayFunctionality.printArray(generatedArray);
+		}
+		
+
+	}
+
+}
