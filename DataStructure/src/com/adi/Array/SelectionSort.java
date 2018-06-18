78443b7fd6e3f8b458092bf2480dfb06a0668085
Aditya <adi@adi.com>
2018-06-18 12:06:40 +0530

    Updating Selection Sort

--- DataStructure/src/com/adi/Array/SelectionSort.java
+++ DataStructure/src/com/adi/Array/SelectionSort.java
@@ -2,15 +2,20 @@ package com.adi.Array;
 
 public class SelectionSort {
 
+	//static int[] generatedArray = new int[20]; 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] generatedArray = new int[20];
+		//int[] generatedArray = {10,9,8,8,6,5,4,3,2,1};
 		// GenerateArray generate = new GenerateArray();
 		generatedArray = BasicArrayFunctionality.createArray(10);
 		
 		int arrayLength = generatedArray.length;
+		int indexMin = 0;
+		int indexMax = 0;
 		int arrayMin = 0;
-		int arrayMax = 0;
+		int numberofSwaps = 0;
+		boolean swapNumber = false;
 		//generatedArray = {100,90};
 		BasicArrayFunctionality.printArray(generatedArray);
 		
@@ -18,18 +23,39 @@ public class SelectionSort {
 			arrayMin = generatedArray[i];
 			for(int j =i+1; j< arrayLength ; j++) {
 				if(arrayMin > generatedArray[j]) {
-					arrayMax = arrayMin;
+					indexMax = j;
+					indexMin = i;
 					arrayMin = generatedArray[j];
-					generatedArray[j] = arrayMax;
+					swapNumber = true;
 				}
 			}
-			//arrayMax = generatedArray[i];
-			generatedArray[i] = arrayMin;
+			if(swapNumber) {
+				int temp = generatedArray[indexMin];
+				
+				generatedArray[indexMin] = generatedArray[indexMax];
+				generatedArray[indexMax] = temp;
+				swapNumber = false;
+				numberofSwaps++;
+			}
+			
+			
+			System.out.println("After " + (i+1) + "iteration");
+			BasicArrayFunctionality.printArray(generatedArray);
 		}
 		
 		System.out.println("After selection sort");
+		System.out.println("Total Number of Swaps: " + numberofSwaps);
 		BasicArrayFunctionality.printArray(generatedArray);
 
 	}
+
+//	private static void swap(int indexMin, int indexMax) {
+//		// TODO Auto-generated method stub
+//		int temp = generatedArray[indexMin];
+//		
+//		generatedArray[indexMin] = generatedArray[indexMax];
+//		generatedArray[indexMax] = generatedArray[indexMin];
+//		
+//	}
 
 }
